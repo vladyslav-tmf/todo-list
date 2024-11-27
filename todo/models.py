3600b6170dd15fb01e00fa88a6667833759bbc86
Vladyslav Timofieiev <vladislav.tmf@gmail.com>
2024-11-27 10:45:42 +0200

    Add Task and Tag models to todo app
    - Implemented "Task" and "Tag" models.

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Task(models.Model):
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(null=True, blank=True)
+    is_done = models.BooleanField(default=False)
+    tag = models.ManyToManyField(Tag, related_name="tasks")
+
+    class Meta:
+        ordering = ["-deadline", "-created_at"]
+
+    def __str__(self) -> str:
+        return self.content[:50]
